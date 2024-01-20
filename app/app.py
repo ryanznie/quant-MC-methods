76c1762009a8c7b1a6c2dcb6c03c832b10b340d5
ryanznie <ryanznie@gmail.com>
2024-01-20 18:28:46 -0500

    added simulations

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, request, jsonify
-from app_utils import fetch_data#, plot_data
+from flask import Flask, request, jsonify, render_template
+from logger_setup import logger
+from app_utils import fetch_data, simulate#, plot_data
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('agg')
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 @app.route('/plots/<ticker>/<days>', methods=['POST'])
 def get_plots(ticker, days):
@@ -19,11 +25,42 @@ def get_plots(ticker, days):
         return jsonify({'error': 'Day not provided'}), 400
     
     data = fetch_data(ticker, time_start='2020-01-01', time_end='2020-12-31')
-    result = data[1]
+    if data is None:
+        return jsonify({'error': 'Ticker cannot be found on yFinance'}), 400
+    
+    final_points, num_sim = simulate(data, days, ticker) # return image path too
 
-    return jsonify({'image_url': result,
-                    'date': days}
-                    ), 200
+        # create a static dir if it odes not already exist
+    if not os.path.exists('static'):
+        logger.info(f'OUTPUT DIRECTORY DOES NOT EXIST, CREATING: static')
+        os.mkdir(output_dir)
+
+    # plotting
+    fig, axes = plt.subplots(1, 2, figsize=(12,4))
+    fig.suptitle(f"Simulated {num_sim} times...: {ticker} ({days} days ahead)", fontsize=18, y = 1)
+
+    ## plot histogram
+    axes[0].hist(final_points, bins='auto', density=False)
+    axes[0].set_xlabel("Ending Price")
+    axes[0].set_ylabel("Frequency")
+    axes[0].set_title("Histogram of future prices")
+
+    ## plot boxplot
+    sns.boxplot(data=final_points, orient="h", ax=axes[1])
+    plt.title("Boxplot of future prices")
+    plt.xlabel("Price in USD")
+    
+    plot_url = f'static/{ticker}_sim{days}D' # change output file path here
+    logger.info(f'SAVING PLOT TO: {plot_url}')
+    fig.savefig(f'{plot_url}')
+    plt.close(fig)
+
+    #return render_template('plots.html', image_path=image_path)
+    return jsonify({
+                    'image_url': plot_url,
+                    'ticker': ticker,
+                    'date': days
+                    }), 200
 
 
 if __name__ == '__main__':
--- app/app_utils.py
+++ app/app_utils.py
@@ -1,17 +1,7 @@
 import yfinance as yf
 import pandas as pd
-import logging
-import logging.config
-import sys
-
-
-enable_debug_logging = True
-
-logging.config.fileConfig('logging_config.ini')
-logger = logging.getLogger(__name__)
-
-if enable_debug_logging:
-    logger.setLevel(logging.DEBUG)
+import numpy as np
+from logger_setup import logger
 
 
 def fetch_data(ticker, time_start, time_end):
@@ -27,17 +17,60 @@ def fetch_data(ticker, time_start, time_end):
     
     if df.shape[0] == 0:
         logger.info(f"FETCH UNSUCCESSFUL - SHAPE: {df.shape}")
-        return
+        return None
     else:
         logger.info(f"FETCHED SUCCESSFULLY - SHAPE: {df.shape}")
 
     # calculate mu std
     mu, std = ( (df.iloc[-1]['Close'] - df.iloc[0]['Close']) / df.iloc[0]['Close'] , df['return'].std() )
 
-    if mu != None:
-        total_time = len(df)
-        logger.info(f"From {str(df.index[0])[:10]} to {str(df.index[-1])[:10]}:") # fix with regex?
-        logger.info(f"  Average return in {total_time} market days is {100*mu:.3f}%")
-        logger.info(f"  Standard deviation of returns during this time period is {std:.3f}")
+    total_time = len(df)
+    logger.info(f"From {str(df.index[0])[:10]} to {str(df.index[-1])[:10]}:") # fix with regex?
+    logger.info(f"  Average return in {total_time} market days is {100*mu:.3f}%")
+    logger.info(f"  Standard deviation of returns during this time period is {std:.3f}")
 
     return (df, (mu, std))
+
+
+def simulate(data, time, ticker, num_sim=100):
+    """
+    MC simulates {time} days into the future {num_sim} times using Monte Carlo methods
+    Plots and store results in {output_dir} (default: static)
+    """
+    df = data[0]
+    mu, sigma = data[1]
+
+    S0 = df.iloc[-1]['Close']
+    muS = mu/len(df)
+
+    final_points = []
+
+    logger.info("")
+    logger.info("---------------------------------------------")
+    logger.info(f"Simulating {ticker} {time} days into the future {num_sim} times...")
+
+    # MC simulate num_sim times
+    for sim in range(num_sim):
+        S_pos = [S0]
+        Z = np.random.normal(0,1, size = time)
+        for t in range(0, time):
+            S_pos.append(S_pos[t] + S_pos[t]*(muS + sigma*Z[t]))
+        final_points.append(S_pos[-1])
+    logger.info("Simulations completed")
+
+    
+    # Descriptive statistics for ending points
+    logger.info("")
+    logger.info(f"Calculating descriptive statistics...")
+    average = np.mean(final_points)
+    median = np.percentile(final_points, 50)
+    twentyfive = np.percentile(final_points, 25)
+    seventyfive = np.percentile(final_points, 75)
+
+    logger.info(f"  Average ending price: ${average:.2f}  ({(100*(average-S0)/S0):.2f}%)")
+    logger.info(f"  Median ending price: ${median:.2f}  ({(100*(median-S0)/S0):.2f}%)")
+    logger.info(f"  25th percentile of ending price: ${twentyfive:.2f}  ({(100*(twentyfive-S0)/S0):.2f}%)")
+    logger.info(f"  75th percentile of ending price: ${seventyfive:.2f}  ({(100*(seventyfive-S0)/S0):.2f}%)")
+
+    return final_points, num_sim
+
